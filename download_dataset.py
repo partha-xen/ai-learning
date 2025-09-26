46c66354e5d03a3b615a3508cb4682d931daa948
Partha Ramachandran <partha@parthaxen.ai>
2025-09-25 17:45:04 -0700

    chapter 3 on HF

---
+++ download_dataset.py
@@ -0,0 +1,44 @@
+from datasets import load_dataset
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    DataCollatorWithPadding,
+    Trainer,
+    TrainingArguments,
+)
+
+raw_datasets = load_dataset("glue", "mrpc")
+print("raw_datasets:")
+print(raw_datasets)
+print("raw_datasets['train']['sentence1']:")
+print(raw_datasets["train"]["sentence1"])
+
+# Same as before
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+
+def tokenize_function(example):
+    return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
+
+
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+training_args = TrainingArguments("test-trainer")
+
+# print("tokenized_datasets:")
+# print(tokenized_datasets)
+
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+
+trainer = Trainer(
+    model,
+    training_args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    data_collator=data_collator,
+    processing_class=tokenizer,
+)
+
+trainer.train()
